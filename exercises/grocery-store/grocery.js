903612e311ee25209dede343558b763c86341e64
wright0776 <matwright2010@gmail.com>
2018-02-28 20:25:29 -0700

    completed star wars, grocery, hello, and cia exercises

---
+++ exercises/grocery-store/grocery.js
@@ -0,0 +1,8 @@
+var shopper = {
+    firstName: "Bill",
+    lastName: "Walker",
+    fullName: function(){return firstName + " " + lastName},
+    age: 72,
+    happy: true,
+    groceryCart: ["corn","hay","birdseed","peanut-butter"]
+}
